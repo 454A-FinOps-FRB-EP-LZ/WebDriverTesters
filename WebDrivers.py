65c847b404397a28c89d59d6bdd1db5ac5519cd7
EdgarPalomino <102803203+EdgarPalomino@users.noreply.github.com>
2024-10-25 14:37:13 -0500

    Adding the code for sending multiple requests

--- WebDrivers.py
+++ WebDrivers.py
@@ -1,3 +1,4 @@
+import time
 import re
 from selenium import webdriver
 
@@ -6,22 +7,32 @@ link = "http://web-alb-871117254.us-east-1.elb.amazonaws.com"
 while True:
     link_choice = input("Choose the route to test (\"Memory\" or \"CPU\"):\n")
     if link_choice == "Memory" or link_choice == "CPU":
-        link = link + "/" + link_choice.lower()
+        link = link + "/stress/" + link_choice.lower()
         break
 
-browsers = []
+browser = webdriver.Firefox()
+browser.get(link)
 
-while True:
-    browsers_choice = input("Enter the number of browsers to launch:\n")
-    if re.fullmatch(r"[1-9]\d*", browsers_choice) is not None:
-        browsers = int(browsers_choice)*[webdriver.Firefox()]
-        for browser in browsers:
-            browser.get(link)
-        break
+start_time = time.time()
 
-print("You have launched " + int(len(browsers)) + " browsers.")
+while time.time()-start_time <= 120:
+    browser.refresh()
 
-while True:
-    stop_choice = input("Type \"Stop\" to close the web drivers.\n")
-    for browser in browsers:
-        browser.close()
+
+
+# browsers = []
+
+# while True:
+#     browsers_choice = input("Enter the number of browsers to launch:\n")
+#     if re.fullmatch(r"[1-9]\d*", browsers_choice) is not None:
+#         browsers = int(browsers_choice)*[webdriver.Firefox()]
+#         for browser in browsers:
+#             browser.get(link)
+#         break
+
+# print("You have launched " + int(len(browsers)) + " browsers.")
+
+# while True:
+#     stop_choice = input("Type \"Stop\" to close the web drivers.\n")
+#     for browser in browsers:
+#         browser.close()
