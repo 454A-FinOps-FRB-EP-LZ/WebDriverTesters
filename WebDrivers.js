e68730b793c9e83305079617975be3634918a403
EdgarPalomino <102803203+EdgarPalomino@users.noreply.github.com>
2024-12-13 14:52:19 -0600

    Final version of testing script

--- WebDrivers.js
+++ WebDrivers.js
@@ -1,27 +1,6 @@
 import http from "k6/http";
 import { sleep, check } from "k6";
 
-// export const options = {
-//     duration: "1m",
-//     vus: 10,
-//     cloud: {
-//         projectID: 3721358,
-//         name: 'Test 1'
-//     }
-// }
-
-// export const options = {
-//     stages: [
-//         {duration: "1m", target: 5},
-//         {duration: "1m", target: 20},
-//         {duration: "1m", target: 5}
-//     ],
-//     cloud: {
-//         projectID: 3721358,
-//         name: 'Test 2'
-//     }
-// }
-
 const progressiveTest = [
     {"duration": "90s", "target": 3},
     {"duration": "1m", "target": 10},
@@ -50,10 +29,16 @@ const performanceTests = {
     "Periodic": periodicTest
 }
 
-// Select the type of test ("Progressive", "Dramatic Sustained" or "Periodic")
+// Copy the server link printed by Terraform
 
 const link = "web-alb-1658970598.us-east-1.elb.amazonaws.com";
+
+// Select the route to test ("CPU" or "Memory")
+
 const route = "CPU";
+
+// Select the type of test ("Progressive", "Dramatic Sustained" or "Periodic")
+
 const test = "Periodic";
 
 export const options = {
@@ -73,9 +58,5 @@ export default function() {
     });
 
     sleep(Math.random()*5)
-
-}
-
-export function handleSummary() {
 
 }
